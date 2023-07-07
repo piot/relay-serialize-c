e3f3cd1c40b9cd532ac88280ecaedb5001d67f38
Peter Bjorklund <piot@hotmail.com>
2023-07-07 19:14:35 +0200

    fix: all commands in CmdToString()

--- src/lib/debug.c
+++ src/lib/debug.c
@@ -14,6 +14,16 @@ const char* relaySerializeCmdToString(uint8_t t)
             return "Packet";
         case relaySerializeCmdPacketToClient:
             return "PacketToClient";
+        case relaySerializeCmdConnectRequestToServer:
+            return "Connect Request";
+        case relaySerializeCmdListenRequestToServer:
+            return "Listen Request";
+        case relaySerializeCmdConnectionRequestToClient:
+            return "Connection Request To Client";
+        case relaySerializeCmdListenResponseToClient:
+            return "listen response to client";
+        case relaySerializeCmdConnectResponseToClient:
+            return "connect response to client";
         default: {
             static char buf[128];
             tc_snprintf(buf, 128, "no idea %02X", t);
